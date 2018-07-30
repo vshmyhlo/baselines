da4465b5de04e7d283326a3eb60096627e460f70
Vlad Shmyhlo <aion.stu@gmail.com>
2018-07-30 22:29:02 +0300

    Refactor

--- todo.py
+++ todo.py
@@ -1,15 +1,10 @@
-# TODO: check no appends
 # TODO: _ between conv and 1
-# TODO: args by lines in def
-# TODO: args by lines in call
-# TODO: check everything is correct
-# TODO: no _ before layer name
+# TODO: format args by lines in def
+# TODO: format args by lines in call
 # TODO: check super calls
 # TODO: make dropout optional
-# TODO: use sequantial where possible
+# TODO: use Sequential where possible
 # TODO: use Normalization where possible
-# TODO: how to keep layers in array?
-# TODO: deal with LayerList
 # TODO: remove redundant `name`
 # TODO: use channelwise dropout for models with dropout
 # TODO: use consistent style
